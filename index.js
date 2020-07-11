094f6df91f513c01b8c65faccca1f0f8069c6940
pad19 <pad19051999@gmail.com>
2020-07-11 15:00:43 +0300

    Add method push

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ MyArray.isMyArray = function (array) {
     return array instanceof MyArray;
 };
 
+const myArrayProto = new MyArray();
+myArrayProto.push = function (...item) {
+    for (let i = 0; i < item.length; i++) {
+        this[this.length++] = item[i];
+    }
+    return this.length;
+};
+
+MyArray.prototype = myArrayProto;
+
+const arr = new MyArray();
+arr.push(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+console.log(arr);
